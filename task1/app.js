30262e2b66b9197e9bc6f73c3baa8a0d885e3528
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-07-13 19:49:22 +0400

    ay mellim silmin haaaa

--- task1/app.js
+++ task1/app.js
@@ -1,22 +1,7 @@
-let numbers = [1, 4, 7, 8, 3, 5, 0, 2, 4, 6, 12, 15, 18, 46, 45, 48, 97, 84]
+const eded=prompt("ededi daxil edin")
 
-
-function countEvensAndOdds(arr) {
-    let even = 0
-    let odd = 0
-    arr.forEach(element => {
-        if (element % 2 == 0) {
-             even++ 
-        } else {
-             odd++ 
-        }
-    });
-    console.log(even)
-    console.log(odd)
+for(let i=100;i<1000;i++){
+    if(i%eded===0){
+        console.log(i)
+    }
 }
-
-
-
-
-
-countEvensAndOdds(numbers)
